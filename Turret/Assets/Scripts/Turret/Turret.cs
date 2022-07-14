84ce870d4ef475fe84240cb7878be52a138e7df6
heeyoung <heeyoung8748@gmail.com>
2022-07-14 23:27:40 +0900

    220714 #3 교수님 진짜 모르겠어요
    1. IDLE 상태일 때 회전 안 하도록 변경
    2. 내적 외적 수업시간때 쓴 거 따라씀
    3. 그리고 뭘 해야할지...?

--- Turret/Assets/Scripts/Turret/Turret.cs
+++ Turret/Assets/Scripts/Turret/Turret.cs
@@ -10,12 +10,18 @@ public class Turret : MonoBehaviour
     private bool _canTurretAttack = false;
     private float _elapsedTime = 0.0f;
     private float _stdTime = 0.5f;
-
+    
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if(other.tag == "Player")
         {
+            Vector3 directionVec = (player.position - player.transform.position).normalized;
+            float direc = Vector3.Dot(player.transform.forward, directionVec);// 내적
+            Vector3 distanceVector = player.position - transform.position; // 두 벡터의 거리
+            Vector3 vecCross = Vector3.Cross(transform.forward.normalized, distanceVector.normalized); // 외적
+        
             _canTurretAttack = true;
         }
     }
@@ -46,13 +52,12 @@ public class Turret : MonoBehaviour
     
     void Update()
     {
+        
+        Debug.DrawRay(transform.position, transform.forward * 5f, Color.blue);
+
         if(_canTurretAttack && player.gameObject.activeSelf)
         {
             TurretAttack();
-        }
-        else
-        {
-            TurretSpin();
         }
         
     }
